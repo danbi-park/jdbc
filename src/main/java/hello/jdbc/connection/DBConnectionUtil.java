55639fe4dc0044fb02aed6ba4548be0b4197dbfe
dbpark@maprex.co.kr <dbpark@maprex.co.kr>
2024-07-14 18:58:41 +0900

    Section_1: JDBC의 이해

---
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,24 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class DBConnectionUtil {
+    public static Connection getConnection(){
+        try {
+            //jdbc가 제공하는 jdbc 드라이버 매니저가 h2의 Driver가 찾아 실제 커넥션을 가져옴
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            /// external library의 h2의 /org/h2/jdbc/JdbcConnection.class를 가져오는거임
+            log.info("get connection={}, class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -8,33 +8,37 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
+
+    // crud를 한번에 테스트 하는 건 편하긴 한데, 한 번 예외가 발생하면 다음 작업이 진행이 안된다. 이 부분은 이후 TRANSACTION으로 처리한다.
     @Test
     void crud() throws SQLException {
-        //save
-        Member member = new Member("memberV100", 10000);
+        // save
+        Member member = new Member("danbi101", 100000);
         repository.save(member);
 
-        //findById
+        // findById
         Member findMember = repository.findById(member.getMemberId());
-        log.info("findMember={}", findMember);
-        assertThat(findMember).isEqualTo(member);
+        log.info("findMember ={}", findMember);
+        log.info("member != findMember : {}", member == findMember); // 다른 인스턴스라서 false
+        log.info("member equals findMember : {}", member.equals(findMember)); // lombok @Data에서는 equalsandhashcode를 포함하기 때문에 true로 반환되는 것임
+//        assertThat(findMember).isEqualTo(member);
 
-        //update: money: 10000 -> 20000
+        //update
         repository.update(member.getMemberId(), 20000);
         Member updatedMember = repository.findById(member.getMemberId());
         assertThat(updatedMember.getMoney()).isEqualTo(20000);
 
+
         //delete
         repository.delete(member.getMemberId());
-        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
-                .isInstanceOf(NoSuchElementException.class);
+        assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 }
